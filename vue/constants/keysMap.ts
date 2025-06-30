2a3dcd924e01f3237c64d1517807d61c7c4e1efb
HelloSkills <HelloSkillsWeb@gmail.com>
2025-06-30 15:33:15 +0300

    Fix destructuring

---
+++ vue/constants/keysMap.ts
@@ -0,0 +1,15 @@
+const keysMap = {
+    'GIT': 'GIT',
+    'JS': 'JS',
+    'Objects': 'OBJECT',
+    'Arrays': 'ARRAYS',
+    'Styles': 'STYLES',
+    'Vue': 'VUE',
+    'Nuxt': 'NUXT',
+    'Links': 'LINKS',
+    'Hotkeys': 'HOTKEYS',
+}
+
+const keys = Object.values(keysMap)
+
+export { keysMap, keys }
